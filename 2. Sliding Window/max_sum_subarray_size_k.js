e838f07e1e7dcc27f5972d03084bb72d4ff89596
davidwt5 <davidwtan75@gmail.com>
2022-05-03 14:46:31 +1000

    initial comit

---
+++ 2. Sliding Window/max_sum_subarray_size_k.js
@@ -0,0 +1,5 @@
+function max_sum_subarray_size_k() {
+  return 0;
+}
+
+module.exports = max_sum_subarray_size_k;
---
+++ 2. Sliding Window/max_sum_subarray_size_k.test.js
@@ -0,0 +1,5 @@
+const max_sum_subarray_size_k = require("./max_sum_subarray_size_k");
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(max_sum_subarray_size_k()).toBe(0);
+});
